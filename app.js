131cada211046cae35f46e45c5665aaa0b8ff151
Rasheed Ahmed <110893684+codewithrasheed@users.noreply.github.com>
2022-10-30 21:59:25 +0500

    Update app.js

--- app.js
+++ app.js
@@ -1,95 +1,95 @@
 // Chapter 1 Practice
 
-alert("Rasheed Ahmed Assignment");
-alert("Welcome to JavaScript")
+// alert("Rasheed Ahmed Assignment");
+// alert("Welcome to JavaScript")
 
 // x-------------x-------------x
 
-// Chapter 2 Practice
-var name1 = "Mark";
-var name2 = "My nationality is U.S";
-console.log(name1);
-console.log(name2);
-// x-------------x-------------x
-
-console.log("------------------------------------");
-
-// Chapter 3 Practice
-var originalNum = 23;
-var newNum = originalNum + 7;
-console.log(newNum)
-// x-------------x-------------x
+// // Chapter 2 Practice
+// var name1 = "Mark";
+// var name2 = "My nationality is U.S";
+// console.log(name1);
+// console.log(name2);
+// // x-------------x-------------x
 
 // console.log("------------------------------------");
 
-// Chapter 4 Practice
-// var abc  //legal
-// var 123 //illegal
-// var 12ab //illegal
-// var ab12 //legal
-// var _  //legal
-// var $ //legal
-// var $abc //legal
-// var $543 //legal
-// var alert //illegal
-// var console //illegal
-// x-------------x-------------x
+// // Chapter 3 Practice
+// var originalNum = 23;
+// var newNum = originalNum + 7;
+// console.log(newNum)
+// // x-------------x-------------x
 
-console.log("------------------------------------");
+// console.log("------------------------------------");
 
-// Chapter 5 Practice
-var a = 10;
-var b = 20;
-var c = a + b;
-console.log("Addition Operator:", c);
-var d = a - b;
-console.log("Subtraction Operator:", d);
-var e = a * b;
-console.log("Multiplication Operator:", e);
-var h = a / b;
-console.log("Division Operator:", h);
-// x-------------x-------------x
+// // Chapter 4 Practice
+// // var abc  //legal
+// // var 123 //illegal
+// // var 12ab //illegal
+// // var ab12 //legal
+// // var _  //legal
+// // var $ //legal
+// // var $abc //legal
+// // var $543 //legal
+// // var alert //illegal
+// // var console //illegal
+// // x-------------x-------------x
 
-console.log("------------------------------------");
+// console.log("------------------------------------");
 
-// Chapter 6 Practice
-var x = 10
-var y = 20
-x++;
-y--;
-console.log(x);
-console.log(y)
-var g = 10;
-var f = g++;
-console.log(f);
-console.log(f);
-// x-------------x-------------x
-console.log("------------------------------------");
+// // Chapter 5 Practice
+// var a = 10;
+// var b = 20;
+// var c = a + b;
+// console.log("Addition Operator:", c);
+// var d = a - b;
+// console.log("Subtraction Operator:", d);
+// var e = a * b;
+// console.log("Multiplication Operator:", e);
+// var h = a / b;
+// console.log("Division Operator:", h);
+// // x-------------x-------------x
 
-// Chapter 7 Practice
-var n = 10;
-var m = 20;
-var o = (n+m) + n;
-console.log("Addition ambiguity Operator:", o);
-var t = (n+m) - n;
-console.log("Subtraction ambiguity Operator:", t);
-var p = (n+m) * n;
-console.log("Multiplication ambiguity Operator:", p);
-var l = (n+m) / n;
-console.log("Division ambiguity Operator:", l);
+// console.log("------------------------------------");
 
-var w = n++ - --m - m + --n + m++;
-console.log("Unfamiliar operators ambiguity", w);
-// x-------------x-------------x
-console.log("------------------------------------");
+// // Chapter 6 Practice
+// var x = 10
+// var y = 20
+// x++;
+// y--;
+// console.log(x);
+// console.log(y)
+// var g = 10;
+// var f = g++;
+// console.log(f);
+// console.log(f);
+// // x-------------x-------------x
+// console.log("------------------------------------");
 
-// Chater 8 Practice
-var message = "Thanks";
-var message1 = "User";
-console.log(message + message1);
-var number = 10;
-var number2 = "20";
-console.log(number + number2);
+// // Chapter 7 Practice
+// var n = 10;
+// var m = 20;
+// var o = (n+m) + n;
+// console.log("Addition ambiguity Operator:", o);
+// var t = (n+m) - n;
+// console.log("Subtraction ambiguity Operator:", t);
+// var p = (n+m) * n;
+// console.log("Multiplication ambiguity Operator:", p);
+// var l = (n+m) / n;
+// console.log("Division ambiguity Operator:", l);
+
+// var w = n++ - --m - m + --n + m++;
+// console.log("Unfamiliar operators ambiguity", w);
+// // x-------------x-------------x
+// console.log("------------------------------------");
+
+// // Chater 8 Practice
+// var message = "Thanks";
+// var message1 = "User";
+// console.log(message + message1);
+// var number = 10;
+// var number2 = "20";
+// console.log(number + number2);
 // x-------------x-------------x
 
 
